4c536f6d38dc3cfe765a603f894d0be51f543816
Duck-citizenf <81924621+Duck-citizenf@users.noreply.github.com>
2021-08-14 21:26:23 +0300

    Add files via upload

--- product_js.js
+++ product_js.js
@@ -14,3 +14,27 @@ function move3() {
     document.getElementById("o1").classList.remove('gallery', 'gallery-2', 'gallery-3');
     document.getElementById("o1").classList.add('gallery-3');
 }
+let a = 0;
+window.onload = function() {
+    document.querySelector('.gallery').addEventListener('swiped-left', function(e) {
+        console.log(e.type);
+        console.log(e.target);
+        console.log(e.detail);
+        if (a==0){document.getElementById("o1").classList.remove('gallery', 'gallery-2', 'gallery-3');
+        document.getElementById("o1").classList.add('gallery-2');}
+        if (a==1){document.getElementById("o1").classList.remove('gallery', 'gallery-2', 'gallery-3');
+        document.getElementById("o1").classList.add('gallery-3');}
+        a +=1;
+    });
+
+    document.querySelector('.gallery').addEventListener('swiped-right', function(e) {
+        console.log(e.type);
+        console.log(e.target);
+        console.log(e.detail);
+        if (a==1){document.getElementById("o1").classList.remove('gallery', 'gallery-2', 'gallery-3');
+        document.getElementById("o1").classList.add('gallery');}
+        if (a==2){document.getElementById("o1").classList.remove('gallery', 'gallery-2', 'gallery-3');
+        document.getElementById("o1").classList.add('gallery-2');}
+        a +=-1;
+    });
+}
