7d7b355dfbc02a2653483d40df5d3fd34cc17dd4
Duck-citizenf <81924621+Duck-citizenf@users.noreply.github.com>
2022-01-19 22:27:45 +0300

    Add files via upload

--- magazin-container.js
+++ magazin-container.js
@@ -1,4 +1,7 @@
 window.addEventListener("resize", resize);
+if (screen.width <= 767){
+    document.getElementById("o2").classList.remove('container');
+}
 function resize(){
     if (screen.width > 767){
         document.getElementById("o2").classList.add('container');
